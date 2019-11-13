5a9a8e2ae1da0c13364677de124952993bd2c529
Rommel Haynes <rhaynes3k@gmail.com>
2019-11-13 19:14:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,7 +19,7 @@ def assign_rooms(speakers)
 end
 
 def printer(batch_badge_creator)
-  batch_badge_creator(array_of_names)
+  batch_badge_creator
   batch_badge_creator.each do |badge|
     puts badge
   end
