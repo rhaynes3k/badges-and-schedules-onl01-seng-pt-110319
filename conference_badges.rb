93d9ce9126131d37b22d527b794290175bb181c6
Rommel Haynes <rhaynes3k@gmail.com>
2019-11-13 18:15:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array_of_names)
+  arr_of_badges = []
+  array_of_names.each do |name|
+  arr_of_badges << "Hello, my name is #{name}."
+end
+arr_of_badges  
+end
+
+def assign_rooms(speakers)
+  rooms = []
+  speakers.each_with_index do |name, index|
+  rooms << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
+  rooms
+end
+
+def printer(attendees)
+  batch_badge_creator.each do |badge|
+    puts badge
+  end
+  badge
+  assign_rooms.each do |rms|
+    puts rms
+  end
+  rms
+end
 # Write your code here.
